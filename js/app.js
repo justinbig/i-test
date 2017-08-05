e7011533d325699c690e2e19a342263ec30b5ca1
justin <justinbig2003@gmail.co>
2017-08-05 09:33:52 +0200

    i-tets

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+$(".slick").slick({
+	fade: true,
+	autoplay: true,
+	autoplayspeed: 2000,
+	arrows: false,
+	dots: true
+})
+
+var $select = $("<select></select>");
+$("header").append($select);
+
+
+$("nav a").each(function(){
+  var $anchor = $(this);
+ 
+  var $option = $("<option></option>");
+
+ 
+  if($anchor.parent().hasClass("active")) {
+    $option.prop("selected", true);
+  }
+ 
+  $option.val($anchor.attr("href"));
+
+  $option.text($anchor.text());
+  
+  $select.append($option);
+});
+
+
+$select.change(function(){
+  
+  window.location = $select.val();
+});
+
+
