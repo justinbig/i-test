04ec64c5521bcc8da80aa91f99af0db73ea7079a
justin <justinbig2003@gmail.co>
2017-08-06 11:08:02 +0200

    i-test

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 
 $(".slick").slick({
-	fade: true,
-	autoplay: true,
-	autoplayspeed: 2000,
-	arrows: false,
-	dots: true
-})
+  fade: true,
+  autoplay: true,
+  autoplayspeed: 2000,
+  arrows: false,
+  dots: true
+});
+
+$("img").css({
+  "display": "none"
+}).fadeIn(2000);
+
+// dropdown menu
 
 var $select = $("<select></select>");
 $("header").append($select);
 
 
 $("nav a").each(function(){
-  var $anchor = $(this);
+   var $anchor = $(this);
  
-  var $option = $("<option></option>");
+   var $option = $("<option></option>");
 
  
-  if($anchor.parent().hasClass("active")) {
+   if($anchor.parent().hasClass("active")) {
     $option.prop("selected", true);
-  }
+    }
  
-  $option.val($anchor.attr("href"));
+   $option.val($anchor.attr("href"));
 
-  $option.text($anchor.text());
+   $option.text($anchor.text());
   
-  $select.append($option);
+   $select.append($option);
 });
 
 
@@ -35,3 +41,29 @@ $select.change(function(){
 });
 
 
+
+// google map
+
+var mapOptions = {
+    center: new google.maps.LatLng(-29.8588,31.0064),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+var markerOptions = {
+    position: new google.maps.LatLng(-29.8588, 31.0064)
+};
+var marker = new google.maps.Marker(markerOptions);
+marker.setMap(map);
+var infoWindowOptions = {
+    content: 'Berea is here'
+};
+
+var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+google.maps.event.addListener(marker,'click',function(e){
+  
+  infoWindow.open(map, marker);
+  
+});
